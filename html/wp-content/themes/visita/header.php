8ddd5a9caa8b4faa1bace0007be6038773adced4
xpark media <webmaster@xparkmedia.com>
2018-08-02 19:47:57 -0700

    fix ie

--- html/wp-content/themes/visita/header.php
+++ html/wp-content/themes/visita/header.php
@@ -12,9 +12,7 @@
 */
 ?>
 <!doctype html>
-<!--[if lt IE 7 ]> <html class="no-js ie6" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7 ]>    <html class="no-js ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8 ]>    <html class="no-js ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8 ]>   <html class="no-js ie8" <?php language_attributes(); ?>> <!--[endif]-->
 <!--[if (gte IE 9)|!(IE)]>--> <html class="no-js" <?php language_attributes(); ?>> <!--[endif]-->
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
